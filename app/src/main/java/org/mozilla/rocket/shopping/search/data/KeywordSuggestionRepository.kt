177caa08d459e7e738f0f363672de8b2cded81cc
Benjamin Cheng <chengwc@gmail.com>
2020-03-02 17:27:31 +0800

    Flipkart API for search terms spike

--- app/src/main/java/org/mozilla/rocket/shopping/search/data/KeywordSuggestionRepository.kt
+++ app/src/main/java/org/mozilla/rocket/shopping/search/data/KeywordSuggestionRepository.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.text.TextUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import mozilla.components.concept.fetch.MutableHeaders
 import mozilla.components.concept.fetch.Request
-import org.json.JSONArray
+import org.json.JSONObject
 import org.mozilla.focus.search.SearchEngineManager
 import org.mozilla.rocket.content.Result
 import org.mozilla.rocket.util.safeApiCall
 import org.mozilla.rocket.util.sendHttpRequest
+import java.net.URLEncoder
 
 class KeywordSuggestionRepository(appContext: Context) {
 
@@ -18,7 +20,15 @@ class KeywordSuggestionRepository(appContext: Context) {
     suspend fun fetchSuggestions(keyword: String): Result<List<String>> = withContext(Dispatchers.IO) {
         return@withContext safeApiCall(
             call = {
-                sendHttpRequest(request = Request(url = getSuggestionApiEndpoint(keyword), method = Request.Method.GET),
+                sendHttpRequest(
+                    request = Request(
+                        url = getSuggestionApiEndpoint(keyword),
+                        method = Request.Method.GET,
+                        headers = MutableHeaders().apply {
+                            set("Fk-Affiliate-Id", "xxxx")
+                            set("Fk-Affiliate-Token", "xxxx")
+                        }
+                    ),
                     onSuccess = {
                         Result.Success(parseSuggestionResult(it.body.string()))
                     },
@@ -32,18 +42,19 @@ class KeywordSuggestionRepository(appContext: Context) {
     }
 
     private fun getSuggestionApiEndpoint(keyword: String): String {
-        return searchEngine.buildSearchSuggestionUrl(keyword)
+        return "https://affiliate-api.flipkart.net/affiliate/1.0/search.json?query=${URLEncoder.encode(keyword, "UTF-8")}&resultCount=$MAX_SUGGESTION_COUNT"
     }
 
     private fun parseSuggestionResult(response: String): List<String> {
         val suggestions = arrayListOf<String>()
         if (!TextUtils.isEmpty(response)) {
-            val jsonArray = JSONArray(response)
-            val suggestionItems = jsonArray.getJSONArray(1)
+            val jsonObject = JSONObject(response)
+            val suggestionItems = jsonObject.getJSONArray("products")
             val size = suggestionItems.length()
 
-            for (i in 0 until size.coerceAtMost(MAX_SUGGESTION_COUNT)) {
-                suggestions.add(suggestionItems.getString(i))
+            for (i in 0 until size) {
+                val product = suggestionItems.getJSONObject(i)
+                suggestions.add(product.getJSONObject("productBaseInfoV1").getString("title"))
             }
         }
         return suggestions
